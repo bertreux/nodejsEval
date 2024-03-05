63da2f9af4cdcecb68380af45bc7d2f6fba5f518
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-05 15:25:14 +0100

    Changement de la nav et des routes

--- router/epreuve_router.js
+++ router/epreuve_router.js
@@ -1,8 +1,8 @@
 import express from "express";
-import {calendrierEpreuve2024} from "../controller/epreuve_controller.js";
+import {getAllEpreuve} from "../controller/epreuve_controller.js";
 
 const epreuve_router = express.Router();
 
-epreuve_router.get('/calendrier', calendrierEpreuve2024);
+epreuve_router.get('/epreuves', getAllEpreuve);
 
 export default epreuve_router;
