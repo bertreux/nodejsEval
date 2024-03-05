3c321c585bd4368295910bfd7b8593130e32bc2a
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-05 16:50:16 +0100

    Ajout des repository
    Création de connexion_repository.js
    
    Création de epreuve_repository.js et des fonction pour avoir toutes les epreuves et des epreuves par un sport
    
    Création de sport_repository.js et d'une fonction pour avoir tous les sports
    
    Mise ajour des templates de base de sports et epreuves pour afficher la liste des données
    
    Création d'une nouvelle page pour afficher toutes les epreuves d'un sport

--- controller/epreuve_controller.js
+++ controller/epreuve_controller.js
@@ -1,5 +1,10 @@
-const getAllEpreuve = (req, res) => {
-    return res.render('front/epreuve/liste_epreuve.njk');
+import {get_all_epreuves} from "../repositories/epreuve_repository.js";
+
+const getAllEpreuve = async (req, res) => {
+    const epreuves = await get_all_epreuves();
+    return res.render('front/epreuve/liste_epreuve.njk', {
+        'epreuves': epreuves,
+    });
 }
 
 export { getAllEpreuve };
--- controller/sport_controller.js
+++ controller/sport_controller.js
@@ -1,5 +1,19 @@
-const getAllSports = (req, res) => {
-    return res.render('front/sport/liste_sports.njk');
+import {get_all_sports} from "../repositories/sport_repository.js";
+import {get_epreuve_by_sport_id} from "../repositories/epreuve_repository.js";
+
+const getAllSports = async (req, res) => {
+    const sports = await get_all_sports();
+    return res.render('front/sport/liste_sports.njk', {
+        'sports': sports,
+    });
 }
 
-export { getAllSports };
+const EpreuveOfSport = async (req, res) => {
+    const { id } = { ...req.params };
+    const epreuves = await get_epreuve_by_sport_id(id);
+    return res.render('front/sport/epreuve_sport.njk', {
+        'epreuves': epreuves,
+    });
+}
+
+export { getAllSports, EpreuveOfSport };
