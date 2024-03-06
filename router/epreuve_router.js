bc03ff1e61bba9364ac8a0e8ea4ce55ff3703243
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-06 15:18:58 +0100

    Ajout de l'index back crud de epreuve

--- router/epreuve_router.js
+++ router/epreuve_router.js
@@ -1,8 +1,10 @@
 import express from "express";
-import {getAllEpreuve} from "../controller/epreuve_controller.js";
+import {getAllEpreuve, tableauOfEpreuveBack} from "../controller/epreuve_controller.js";
 
 const epreuve_router = express.Router();
 
-epreuve_router.get('/epreuves', getAllEpreuve);
+epreuve_router.get('/epreuves/', getAllEpreuve);
+
+epreuve_router.get('/gestion/epreuves/', tableauOfEpreuveBack);
 
 export default epreuve_router;
