5f1a748531ab13fef7864c3f6b6cd580d90aed69
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-06 15:20:06 +0100

    mise a jour des url

--- router/connexion_router.js
+++ router/connexion_router.js
@@ -3,10 +3,10 @@ import {connexion, deconnexion, inscription} from "../controller/connexion_contr
 
 const connexion_router = express.Router();
 
-connexion_router.get('/connexion', connexion);
+connexion_router.get('/connexion/', connexion);
 
-connexion_router.get('/deconnexion', deconnexion);
+connexion_router.get('/deconnexion/', deconnexion);
 
-connexion_router.get('/inscription', inscription);
+connexion_router.get('/inscription/', inscription);
 
 export default connexion_router;
