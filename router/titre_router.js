44cccf17d90693f636d5d665600739b586dbaaa2
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-06 16:35:11 +0100

    Début form edit new de titre

--- router/titre_router.js
+++ router/titre_router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getAllTitres, tableauOfTitreBack, showOfTitreBack} from "../controller/titre_controller.js";
+import {getAllTitres, tableauOfTitreBack, showOfTitreBack, editOfTitreBack, newOfTitreBack} from "../controller/titre_controller.js";
 import epreuve_router from "./epreuve_router.js";
 
 const titre_router = express.Router();
@@ -11,9 +11,9 @@ titre_router.get('/gestion/titres/', tableauOfTitreBack);
 epreuve_router.get('/gestion/titre/:id/show', showOfTitreBack);
 
 epreuve_router.get('/gestion/titre/:id/edit', editOfTitreBack);
-epreuve_router.post('/gestion/titre/:id/edit', insertDataEditOfTitreBack);
+//epreuve_router.post('/gestion/titre/:id/edit', insertDataEditOfTitreBack);
 
-epreuve_router.get('/gestion/titre/:id/new', newOfTitreBack);
-epreuve_router.post('/gestion/titre/:id/new', insertDataNewOfTitreBack);
+epreuve_router.get('/gestion/titre/new', newOfTitreBack);
+//epreuve_router.post('/gestion/titre/new', insertDataNewOfTitreBack);
 
 export default titre_router;
