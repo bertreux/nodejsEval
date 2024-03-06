c1ac0c37d0d582c068108f26b9f41e7a1b09e623
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-06 15:47:06 +0100

    Ajout d'une route show pour afficher un sport et creation methode pour trouver un sport par un id

--- router/sport_router.js
+++ router/sport_router.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {getAllSports, EpreuveOfSport, tableauOfSportBack} from "../controller/sport_controller.js";
+import {getAllSports, EpreuveOfSport, tableauOfSportBack, showOfSportBack} from "../controller/sport_controller.js";
+import epreuve_router from "./epreuve_router.js";
 
 const sport_router = express.Router();
 
@@ -8,5 +9,7 @@ sport_router.get('/sports/', getAllSports);
 sport_router.get('/sport/:id/', EpreuveOfSport);
 
 sport_router.get('/gestion/sports/', tableauOfSportBack);
+
+epreuve_router.get('/gestion/sport/:id/show', showOfSportBack);
 
 export default sport_router;
