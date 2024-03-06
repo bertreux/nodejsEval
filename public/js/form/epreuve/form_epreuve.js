2eb80e8c6a0dfcb56bcd07d829c28abd82c14b58
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-06 22:46:05 +0100

    Création du js pour les form
    Creer les fonction pour supprimer, modifier et/ou supprimer un sport

--- public/js/form/epreuve/form_epreuve.js
+++ public/js/form/epreuve/form_epreuve.js
@@ -1,4 +1,4 @@
 import sendFormValueAndReturnResponse from "../form.js";
 
 const type_form = document.getElementById('type_url_api').value;
-sendFormValueAndReturnResponse(`gestion/epreuve/${type_form}`);
+sendFormValueAndReturnResponse(`gestion/epreuve/${type_form}`, 'Created', 'Error');
--- router/sport_router.js
+++ router/sport_router.js
@@ -1,6 +1,7 @@
 import express from "express";
-import {getAllSports, EpreuveOfSport, tableauOfSportBack, showOfSportBack, editOfSportBack, newOfSportBack} from "../controller/sport_controller.js";
+import {getAllSports, EpreuveOfSport, tableauOfSportBack, showOfSportBack, editOfSportBack, newOfSportBack, deleteOfSportBack, insertDataEditOfSportBack, insertDataNewOfSportBack} from "../controller/sport_controller.js";
 import epreuve_router from "./epreuve_router.js";
+import athlete_router from "./athlete_router.js";
 
 const sport_router = express.Router();
 
@@ -10,12 +11,14 @@ sport_router.get('/sport/:id/', EpreuveOfSport);
 
 sport_router.get('/gestion/sports/', tableauOfSportBack);
 
+athlete_router.get('/gestion/sport/:id/sup', deleteOfSportBack);
+
 epreuve_router.get('/gestion/sport/:id/show', showOfSportBack);
 
 epreuve_router.get('/gestion/sport/:id/edit', editOfSportBack);
-//epreuve_router.post('/gestion/sport/:id/edit', insertDataEditOfSportBack);
+epreuve_router.post('/gestion/sport/:id/edit', insertDataEditOfSportBack);
 
 epreuve_router.get('/gestion/sport/new', newOfSportBack);
-//epreuve_router.post('/gestion/sport/new', insertDataNewOfSportBack);
+epreuve_router.post('/gestion/sport/new', insertDataNewOfSportBack);
 
 export default sport_router;
