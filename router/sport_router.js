87badd53b7b0d21a583e195923c72b022647b23e
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-06 15:19:21 +0100

    Ajout de l'index back crud de sport

--- router/sport_router.js
+++ router/sport_router.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {getAllSports, EpreuveOfSport} from "../controller/sport_controller.js";
+import {getAllSports, EpreuveOfSport, tableauOfSportBack} from "../controller/sport_controller.js";
 
 const sport_router = express.Router();
 
-sport_router.get('/sports', getAllSports);
+sport_router.get('/sports/', getAllSports);
 
-sport_router.get('/sport/:id', EpreuveOfSport);
+sport_router.get('/sport/:id/', EpreuveOfSport);
+
+sport_router.get('/gestion/sports/', tableauOfSportBack);
 
 export default sport_router;
