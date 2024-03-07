064f7090db65504433b9bcdb6d789598ad4632c5
Pozymandiaz <lucasverstraete01@gmail.com>
2024-03-07 13:55:39 +0100

    ajout requêtes
    requêtes pour athlete/pays et participation/medaille

--- repositories/athlete_repository.js
+++ repositories/athlete_repository.js
@@ -26,7 +26,7 @@ const get_one_athlete_by_id = async (id) => {
 const insert_new_athlete = async (data) => {
     const db = await db_connection();
     try {
-        const [ results ] = await db.query('INSERT INTO jeux_olympiques.athlete VALUE(NULL, :nom, :prenom, :age, :sexe, :pays_id)',
+        const [ results ] = await db.query('INSERT INTO jeux_olympiques.athlete VALUE(NULL, :nom, :prenom, :age, :sexe)',
             data)
         return results;
     } catch (error) {
@@ -38,7 +38,7 @@ const update_athlete_by_id = async (id, data) => {
     const db = await db_connection();
     data.id_athlete = id;
     try {
-        const [ results ] = await db.query('UPDATE jeux_olympiques.athlete SET athlete.nom = :nom, athlete.prenom = :prenom, athlete.age = :age, athlete.sexe = :sexe, athlete.pays_id = :pays_id where athlete.id_athlete = :id_athlete',
+        const [ results ] = await db.query('UPDATE jeux_olympiques.athlete SET athlete.nom = :nom, athlete.prenom = :prenom, athlete.age = :age, athlete.sexe = :sexe where athlete.id_athlete = :id_athlete:',
             data)
         return results;
     } catch (error) {
@@ -59,4 +59,31 @@ const delete_athlete_by_id = async (id) => {
     }
 };
 
-export { get_all_athletes, get_one_athlete_by_id, insert_new_athlete, update_athlete_by_id, delete_athlete_by_id };
+const get_athlete_medaille = async (id) => {
+    const db = await db_connection();
+    try {
+        const [ results ] = await db.query('SELECT a.nom AS nom_athlete, e.Nom, m.couleur FROM athletes a JOIN participation p ON a.id_athlete = p.athlete_id JOIN epreuve e ON p.epreuve_id = e.id_epreuve LEFT JOIN medaille m ON p.medaille_id = m.id_medaille;',
+            {
+                id: id,
+            })
+        return results;
+    } catch (error) {
+        return error;
+    }
+};
+
+const get_athlete_pays = async (id) => {
+    const db = await db_connection();
+    try {
+        const [ results ] = await db.query('SELECT a.id_athlete, a.Nom, a.Prenom, a.Age, p.Nom AS nom_pays FROM athletes a JOIN pays p ON a.pays_id = p.id_pays;',
+            {
+                id: id,
+            })
+        return results;
+    } catch (error) {
+        return error;
+    }
+};
+
+
+export { get_all_athletes, get_one_athlete_by_id, insert_new_athlete, update_athlete_by_id, delete_athlete_by_id, get_athlete_medaille };
