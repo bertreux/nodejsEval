6ba45c34c742591e59113c4a19d5bb63fff3d354
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-08 18:58:40 +0100

    Amélioration script de la carte

--- public/js/carte/carte_sport.js
+++ public/js/carte/carte_sport.js
@@ -9,9 +9,9 @@ addEventListener("DOMContentLoaded", async (event) => {
     });
     const request = await fetch(requestInfos);
     const response = await request.json();
-    if(response.status === 200){
+    if (response.status === 200) {
         console.log(response.sports)
-        var map = L.map('map').setView([48, 2], 13);
+        var map = L.map('map').setView([48.8588255,2.2646345], 13);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -21,7 +21,7 @@ addEventListener("DOMContentLoaded", async (event) => {
         response.sports.map((sport) => {
             var marker = L.marker([sport.latitude, sport.longitude]).addTo(map);
 
-            marker.bindPopup(`<b>${sport.Nom}</b>`).openPopup();
+            marker.bindTooltip(sport.Nom); // Ajout de la tooltip avec le nom du sport
 
             marker.on('click', (e) => {
                 location.href = `/sport/${sport.id_sport}`;
@@ -29,6 +29,3 @@ addEventListener("DOMContentLoaded", async (event) => {
         })
     }
 });
-
-
-
