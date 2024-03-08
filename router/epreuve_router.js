7b8887eb52c3760c82d81e6481711504a3bde556
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-08 11:08:12 +0100

    Ajout d'un middleware pour vérifier si le user est connecter pour le back sinon va vers une page erreur 403

--- router/epreuve_router.js
+++ router/epreuve_router.js
@@ -1,21 +1,22 @@
 import express from "express";
 import {getAllEpreuve, tableauOfEpreuveBack, showOfEpreuveBack, editOfEpreuveBack, newOfEpreuveBack, deleteOfEpreuveBack, insertDataEditOfEpreuveBack, insertDataNewOfEpreuveBack} from "../controller/epreuve_controller.js";
 import epreuve_form_middleware from "../middleware/form/epreuve_form_middleware.js";
+import {est_connecte} from "../middleware/form/connexion_form_middleware.js";
 
 const epreuve_router = express.Router();
 
 epreuve_router.get('/epreuves/', getAllEpreuve);
 
-epreuve_router.get('/gestion/epreuves/', tableauOfEpreuveBack);
+epreuve_router.get('/gestion/epreuves/', [est_connecte], tableauOfEpreuveBack);
 
-epreuve_router.get('/gestion/epreuve/:id/show', showOfEpreuveBack);
+epreuve_router.get('/gestion/epreuve/:id/show', [est_connecte], showOfEpreuveBack);
 
-epreuve_router.get('/gestion/epreuve/:id/sup', deleteOfEpreuveBack);
+epreuve_router.get('/gestion/epreuve/:id/sup', [est_connecte], deleteOfEpreuveBack);
 
-epreuve_router.get('/gestion/epreuve/:id/edit', editOfEpreuveBack);
-epreuve_router.post('/gestion/epreuve/:id/edit', [epreuve_form_middleware], insertDataEditOfEpreuveBack);
+epreuve_router.get('/gestion/epreuve/:id/edit', [est_connecte], editOfEpreuveBack);
+epreuve_router.post('/gestion/epreuve/:id/edit', [est_connecte, epreuve_form_middleware], insertDataEditOfEpreuveBack);
 
-epreuve_router.get('/gestion/epreuve/new', newOfEpreuveBack);
-epreuve_router.post('/gestion/epreuve/new', [epreuve_form_middleware], insertDataNewOfEpreuveBack);
+epreuve_router.get('/gestion/epreuve/new', [est_connecte], newOfEpreuveBack);
+epreuve_router.post('/gestion/epreuve/new', [est_connecte, epreuve_form_middleware], insertDataNewOfEpreuveBack);
 
 export default epreuve_router;
--- router/sport_router.js
+++ router/sport_router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {getAllSports, EpreuveOfSport, tableauOfSportBack, showOfSportBack, editOfSportBack, newOfSportBack, deleteOfSportBack, insertDataEditOfSportBack, insertDataNewOfSportBack} from "../controller/sport_controller.js";
 import sport_form_middleware from "../middleware/form/sport_form_middleware.js";
+import {est_connecte} from "../middleware/form/connexion_form_middleware.js";
 
 const sport_router = express.Router();
 
@@ -8,16 +9,16 @@ sport_router.get('/sports/', getAllSports);
 
 sport_router.get('/sport/:id/', EpreuveOfSport);
 
-sport_router.get('/gestion/sports/', tableauOfSportBack);
+sport_router.get('/gestion/sports/', [est_connecte], tableauOfSportBack);
 
-sport_router.get('/gestion/sport/:id/sup', deleteOfSportBack);
+sport_router.get('/gestion/sport/:id/sup', [est_connecte], deleteOfSportBack);
 
-sport_router.get('/gestion/sport/:id/show', showOfSportBack);
+sport_router.get('/gestion/sport/:id/show', [est_connecte], showOfSportBack);
 
-sport_router.get('/gestion/sport/:id/edit', editOfSportBack);
-sport_router.post('/gestion/sport/:id/edit', [sport_form_middleware], insertDataEditOfSportBack);
+sport_router.get('/gestion/sport/:id/edit', [est_connecte], editOfSportBack);
+sport_router.post('/gestion/sport/:id/edit', [est_connecte, sport_form_middleware], insertDataEditOfSportBack);
 
-sport_router.get('/gestion/sport/new', newOfSportBack);
-sport_router.post('/gestion/sport/new', [sport_form_middleware], insertDataNewOfSportBack);
+sport_router.get('/gestion/sport/new', [est_connecte], newOfSportBack);
+sport_router.post('/gestion/sport/new', [est_connecte, sport_form_middleware], insertDataNewOfSportBack);
 
 export default sport_router;
